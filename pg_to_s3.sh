ee2d9ca2fb5fb3502fa63a2f2477b3f52370c007
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2016-08-13 17:08:14 -0400

    Remove expiration metadata, use s3-autodelete.sh to delete old files

--- pg_to_s3.sh
+++ pg_to_s3.sh
@@ -14,14 +14,13 @@ dbs=("$@")
 
 # Vars
 NOW=$(date +"%m-%d-%Y-at-%H-%M-%S")
-EXPIRATION_DATE=`date --date="1 week" +%Y-%m-%d`
 
 for db in "${dbs[@]}"; do
     # Dump database
     pg_dump -Fc -h $PG_HOST -U $PG_USER $db > /tmp/"$NOW"_"$db".dump
 
     # Copy to S3
-    aws s3 cp /tmp/"$NOW"_"$db".dump s3://$S3_PATH/"$NOW"_"$db".dump --expires $EXPIRATION_DATE --storage-class STANDARD_IA
+    aws s3 cp /tmp/"$NOW"_"$db".dump s3://$S3_PATH/"$NOW"_"$db".dump --storage-class STANDARD_IA
 
     # Delete local file
     rm /tmp/"$NOW"_"$db".dump
@@ -29,3 +28,7 @@ for db in "${dbs[@]}"; do
     # Log
     echo "* Database $db is archived"
 done
+
+# Delere old files
+echo "* Delete old backups";
+./s3-autodelete.sh $S3_PATH "7 days"
