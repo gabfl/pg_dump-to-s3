773365ca2c0058a4142d31e13aeb6a9275078513
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2016-08-13 17:09:34 -0400

    Change input to require path only

--- s3-autodelete.sh
+++ s3-autodelete.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage:
-# ./s3-autodelete.sh s3://bucket/path/ "7 days"
+# ./s3-autodelete.sh bucket/path "7 days"
 
 set -e
 
@@ -9,19 +9,19 @@ set -e
 maxDate=`date +%s --date="-$2"`
 
 # Loop thru files
-aws s3 ls $1 | while read -r line;  do
+aws s3 ls s3://$1/ | while read -r line;  do
     # Get file creation date
     createDate=`echo $line|awk {'print $1" "$2'}`
     createDate=`date -d"$createDate" +%s`
 
     if [[ $createDate -lt $maxDate ]]
     then
-        # Get file name
+	# Get file name
         fileName=`echo $line|awk {'print $4'}`
         if [[ $fileName != "" ]]
           then
-              echo "* Delete $fileName";
-              aws s3 rm $1"$fileName"
+	      echo "* Delete $fileName";
+	      aws s3 rm s3://$1/$fileName
         fi
     fi
 done;
