ec8eb629ff253fe56e7bd3f77f065933b249cda6
gab <gabrielbordeaux@gmail.com>
2018-05-28 17:50:17 -0400

    Syntax fixes

--- pg_dump-to-s3.sh
+++ pg_dump-to-s3.sh
@@ -20,17 +20,20 @@ source $DIR/pg_dump-to-s3.conf
 
 # Vars
 NOW=$(date +"%Y-%m-%d-at-%H-%M-%S")
-maxDate=`[ "$(uname)" = Linux ] && date +%s --date="-$DELETE_AFTER"` # Maximum date (will delete all files older than this date)
+DELETETION_TIMESTAMP=`[ "$(uname)" = Linux ] && date +%s --date="-$DELETE_AFTER"` # Maximum date (will delete all files older than this date)
 
 # Split databases
 IFS=',' read -ra DBS <<< "$PG_DATABASES"
 
 # Delere old files
-echo " * Backup in progress..";
+echo " * Backup in progress.,.";
 
 # Loop thru databases
 for db in "${DBS[@]}"; do
-    FILENAME="$PREFIX"_"$NOW"_"$db"    
+    FILENAME="$PREFIX"_"$NOW"_"$db"
+
+
+    echo "   -> backing up $db..."
 
     # Dump database
     pg_dump -Fc -h $PG_HOST -U $PG_USER -p $PG_PORT $db > /tmp/"$FILENAME".dump
@@ -42,26 +45,26 @@ for db in "${DBS[@]}"; do
     rm /tmp/"$FILENAME".dump
 
     # Log
-    echo " -> database $db has been backed up"
+    echo "      ...database $db has been backed up"
 done
 
 # Delere old files
 echo " * Deleting old backups...";
 
 # Loop thru files
-aws s3 ls s3://$1/ | while read -r line;  do
+aws s3 ls s3://$S3_PATH/ | while read -r line;  do
     # Get file creation date
     createDate=`echo $line|awk {'print $1" "$2'}`
     createDate=`date -d"$createDate" +%s`
 
-    if [[ $createDate -lt $maxDate ]]
+    if [[ $createDate -lt $DELETETION_TIMESTAMP ]]
     then
-    # Get file name
-        fileName=`echo $line|awk {'print $4'}`
-        if [[ $fileName != "" ]]
+        # Get file name
+        FILENAME=`echo $line|awk {'print $4'}`
+        if [[ $FILENAME != "" ]]
           then
-            echo " -> Deleting $fileName"
-            aws s3 rm s3://$1/$fileName
+            echo "   -> Deleting $FILENAME"
+            aws s3 rm s3://$S3_PATH/$FILENAME
         fi
     fi
 done;
